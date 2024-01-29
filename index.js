0cc3dced3fc1f816e4d79cc5ffe86c890ed3a83c
Luis <158003827+xxxlxlx@users.noreply.github.com>
2024-01-28 19:14:19 -0600

    Create index.js
    Primer Ejercicio JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//     ____.  _________   _________      ____________________________.________________________
+//    |    | /  _  \   \ /   /  _  \    /   _____/\_   ___ \______   \   \______   \__    ___/
+//    |    |/  /_\  \   Y   /  /_\  \   \_____  \ /    \  \/|       _/   ||     ___/ |    |   
+///\__|    /    |    \     /    |    \  /        \\     \___|    |   \   ||    |     |    |   
+//\________\____|__  /\___/\____|__  / /_______  / \______  /____|_  /___||____|     |____|   
+//                 \/              \/          \/         \/       \/                         
+// Script by Lu1s X.
+
+let nombre = 0 
+let edad = 0 
+let genero = 0 
+nombre = prompt("Ingresa tu nombre")
+edad = prompt("Ingresa tu edad")
+genero = prompt("Ingresa tu genero")
+alert ("Tu nombre es:" +nombre)
+alert ("Tu edad es:" +edad)
+alert ("Tu genero:" +genero)
